0befb95709c439ba342a743cc44ad85193272c5e
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-01-17 21:28:13 +0100

    completed 3rd part of 3rd go task

--- Go/2 - annalyns_infiltration.go
+++ Go/2 - annalyns_infiltration.go
@@ -23,6 +23,7 @@ Define the CanFastAttack() function that takes a boolean value that indicates if
 var knightIsAwake = true
 fmt.Println(CanFastAttack(knightIsAwake))
 // Output: false
+
 2. Check if the group can be spied upon
 Define the CanSpy() function that takes three boolean values, indicating if the knight, archer and the prisoner, respectively, are awake. The function returns true if the group can be spied upon, based on the state of the three characters. Otherwise, returns false:
 
@@ -31,6 +32,7 @@ var archerIsAwake = true
 var prisonerIsAwake = false
 fmt.Println(CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake))
 // Output: true
+
 3. Check if the prisoner can be signaled
 Define the CanSignalPrisoner() function that takes two boolean values, indicating if the archer and the prisoner, respectively, are awake. The function returns true if the prisoner can be signaled, based on the state of the two characters. Otherwise, returns false:
 
@@ -38,6 +40,7 @@ var archerIsAwake = false
 var prisonerIsAwake = true
 fmt.Println(CanSignalPrisoner(archerIsAwake, prisonerIsAwake))
 // Output: true
+
 4. Check if the prisoner can be freed
 Define the CanFreePrisoner() function that takes four boolean values. The first three parameters indicate if the knight, archer and the prisoner, respectively, are awake. The last parameter indicates if Annalyn's pet dog is present. The function returns true if the prisoner can be freed based on the state of the three characters and Annalyn's pet dog presence. Otherwise, it returns false:
 
@@ -66,13 +69,22 @@ func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
 	}
 }
 
+func CanSignalPrisoner(prisonerIsAwake, archerIsAwake bool) bool {
+	if !prisonerIsAwake && archerIsAwake {
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
 
 	//fmt.Println("Annalyn")
-	var knightIsAwake = false
-	var archerIsAwake = true
-	var prisonerIsAwake = false
-	fmt.Println(CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake))
+	//var knightIsAwake = false
+	var archerIsAwake = false
+	var prisonerIsAwake = true
+	fmt.Println(CanSignalPrisoner(archerIsAwake, prisonerIsAwake))
+	//fmt.Println(CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake))
 	// fmt.Println(CanFastAttack(knightIsAwake))
 
 }
