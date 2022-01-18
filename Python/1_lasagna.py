d344da5371ec860566bea7ee2e63ae78954270ae
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-01-18 22:16:55 +0100

    fixed typo in python task

--- Python/1_lasagna.py
+++ Python/1_lasagna.py
@@ -36,8 +36,8 @@ Go back through the recipe, adding notes and documentation. """
 
 EXPECTED_BAKE_TIME = 40
 
-def bake_time_remaining(actual_minutes):
-    return EXPECTED_BAKE_TIME - actual_minutes
+def bake_time_remaining(elapsed_bake_time):
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 def preparation_time_in_minutes(number_of_layers):
     return number_of_layers * 2
