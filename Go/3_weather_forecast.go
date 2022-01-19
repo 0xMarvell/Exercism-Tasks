0837573c323a8821cf9a911224337786b370becd
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-01-19 22:48:09 +0100

    completed all parts of 4th go task

--- Go/3_weather_forecast.go
+++ Go/3_weather_forecast.go
@@ -14,3 +14,18 @@ Just like the previous step, write a comment for CurrentLocation.
 
 4. Document the Forecast() function
 Write a function comment for Forecast(). Your comments must describe what the function does, but not how it does it. */
+
+// Package weather provides tools to return the weather forecast for any location it is provided with.
+package weather
+
+// CurrentCondition represents the current weather condition for a specific provided location.
+var CurrentCondition string
+
+// CurrentLocation represents the porvided location to read the weather forecast from.
+var CurrentLocation string
+
+//Forecast takes two string arguments "city" and "condition" and returns a string value that represents the weather forecast for a specific location.
+func Forecast(city, condition string) string {
+	CurrentLocation, CurrentCondition = city, condition
+	return CurrentLocation + " - current weather condition: " + CurrentCondition
+}
