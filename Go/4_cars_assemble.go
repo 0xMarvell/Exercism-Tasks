5fe867c676a34a36991bb904ef64e0606c2b742a
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-01-24 21:06:53 +0100

    completed 2nd part of cars assemble go task

--- Go/4_cars_assemble.go
+++ Go/4_cars_assemble.go
@@ -48,7 +48,7 @@ func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float6
 // produced by the assembly line every minute
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
 	//panic("CalculateWorkingCarsPerMinute not implemented")
-	//return int(CalculateWorkingCarsPerHour(productionRate,successRate)*60)
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) * 60)
 }
 
 // CalculateCost works out the cost of producing the given number of cars
