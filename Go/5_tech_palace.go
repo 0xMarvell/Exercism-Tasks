3f42369941d5e11d9c45fc3a18c5fb34464115ce
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-01-26 21:11:08 +0100

    completed 2nd part of tech palace task (go)

--- Go/5_tech_palace.go
+++ Go/5_tech_palace.go
@@ -37,21 +37,35 @@ CleanUpMessage(message)
 package techpalace
 
 import (
-	"fmt"
 	"strings"
 )
 
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	fmt.Printf("Welcome to the Tech Palace, %s", strings.ToUpper(customer))
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	return strings.Repeat("*", numStarsPerLine) + "\n" + welcomeMsg + "\n" + strings.Repeat("*", numStarsPerLine)
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	//panic("Please implement the CleanupMessage() function")
+	trimStars := strings.ReplaceAll(oldMsg, "*", "")
+	cleanMsg := strings.TrimSpace(trimStars)
+	return cleanMsg
 }
+
+// func main() {
+
+// 	fmt.Println(WelcomeMessage("Marv"))
+// 	fmt.Println(AddBorder("Hello", 10))
+// 	message := `
+// **************************
+// *    BUY NOW, SAVE 10%   *
+// **************************
+// `
+// 	fmt.Print(CleanupMessage(message))
+// }
