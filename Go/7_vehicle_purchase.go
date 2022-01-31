8ecb85a34182e7211ad607b49d560ae45af77ab3
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-01-31 19:59:46 +0100

    completed 1st part of vehicle purchase (go)

--- Go/7_vehicle_purchase.go
+++ Go/7_vehicle_purchase.go
@@ -36,11 +36,15 @@ CalculateResellPrice(1000, 15)
 // Output: 500
 Note the return value is a float64. */
 
-package purchase
+// package purchase
+package main
+
+import "fmt"
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	//panic("NeedsLicense not implemented")
+	return kind == "car" || kind == "truck"
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in dictionary order.
@@ -51,4 +55,10 @@ func ChooseVehicle(option1, option2 string) string {
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
 	panic("CalculateResellPrice not implemented")
+}
+
+func main() {
+
+	needLicense := NeedsLicense("car")
+	fmt.Println(needLicense)
 }
