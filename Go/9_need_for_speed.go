5dd1e320cf9d696606afe2398f8a21dee716b260
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-02-03 13:55:00 +0100

    completed 1st part of need for speed (go)

--- Go/9_need_for_speed.go
+++ Go/9_need_for_speed.go
@@ -50,13 +50,25 @@ CanFinish(car, raceTrack)
 package speed
 
 // TODO: define the 'Car' type struct
+type Car struct {
+	battery, batteryDrain, speed, distance int
+}
 
 // NewCar creates a new remote controlled car with full battery and given specifications.
 func NewCar(speed, batteryDrain int) Car {
-	panic("Please implement the NewCar function")
+	//panic("Please implement the NewCar function")
+	car := Car{
+		battery:      100,
+		speed:        speed,
+		batteryDrain: batteryDrain,
+	}
+	return car
 }
 
 // TODO: define the 'Track' type struct
+type Track struct {
+	distance int
+}
 
 // NewTrack created a new track
 func NewTrack(distance int) Track {
