847dbcc65338a395487e6ff3c1f0e1834b1668c2
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-02-09 20:51:18 +0100

    cleaned up solution to booking for beauty  (go)

--- Go/11_booking_up_for_beauty.go
+++ Go/11_booking_up_for_beauty.go
@@ -42,7 +42,6 @@ import (
 
 // Schedule returns a time.Time from a string containing a date
 func Schedule(date string) time.Time {
-	//panic("Please implement the Schedule function")
 	t, err := time.Parse("1/02/2006 15:04:05", date)
 	if err != nil {
 		panic(err)
@@ -52,7 +51,6 @@ func Schedule(date string) time.Time {
 
 // HasPassed returns whether a date has passed
 func HasPassed(date string) bool {
-	//panic("Please implement the HasPassed function")
 	now := time.Now().UTC()
 	t, err := time.Parse("January 2, 2006 15:04:05", date)
 	if err != nil {
@@ -63,7 +61,6 @@ func HasPassed(date string) bool {
 
 // IsAfternoonAppointment returns whether a time is in the afternoon
 func IsAfternoonAppointment(date string) bool {
-	//panic("Please implement the IsAfternoonAppointment function")
 	t, err := time.Parse("Monday, January 2, 2006 15:04:05", date)
 	if err != nil {
 		panic(err)
@@ -74,7 +71,6 @@ func IsAfternoonAppointment(date string) bool {
 
 // Description returns a formatted string of the appointment time
 func Description(date string) string {
-	//panic("Please implement the Description function")
 	t, err := time.Parse("1/2/2006 15:04:05", date)
 	if err != nil {
 		panic(err)
@@ -92,6 +88,5 @@ func Description(date string) string {
 
 // AnniversaryDate returns a Time with this year's anniversary
 func AnniversaryDate() time.Time {
-	//panic("Please implement the AnniversaryDate function")
 	return time.Date(time.Now().Year(), time.September, 15, 0, 0, 0, 0, time.UTC)
 }
